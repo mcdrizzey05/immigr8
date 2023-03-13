a30945fd11d3266f7cd687d58b168e9da2c6a5bb
Marie Claude <mc.leveille@gmail.com>
2023-03-13 14:46:31 +0100

    Added delete option for comments

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root to: "categories#index"
   get 'articles/user_articles', to: 'articles#user_articles', as: 'user_articles'
   delete 'favourites/:id', to: 'favourites#destroy', as: 'favourite_delete'
+  delete 'comments/:id', to: 'comments#destroy', as: 'comment_delete'
   # Jenni: how to inegrate favourites, messages and chatrooms here?
   resources :users, only: ['show']
   resources :favourites, only: ['index', 'update', 'show']
-  resources :comments, only: ['destroy']
   resources :categories do
     resources :articles, only: ['new', 'create', 'show']
   end
