da2b4bbcf69766a92ba7e47df9a10c0feb172f11
Alina Bich <alina.a.bich@gmail.com>
2023-03-14 16:43:01 +0100

    you can now access chatroom index through message link in navbar and have an actual chatrooms index with all your chats

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,9 +1,10 @@
 class ChatroomsController < ApplicationController
 
-  #def index
-    #@chatroom = Chatroom.current_scope
-    #@message = Message.all
-  #end
+  def index
+    @chatroom = Chatroom.current_scope
+    my_chatrooms = current_user.chatrooms
+    @message = Message.all
+  end
 
   def show
     @chatroom = Chatroom.find(params[:id])
--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,10 @@ class Chatroom < ApplicationRecord
   has_many :chatrooms_users
   has_many :users, through: :chatrooms_users
   has_many :messages
+
+  def filter_current_user(current_user)
+    users.reject do |user|
+      user == current_user
+    end
+  end
 end
