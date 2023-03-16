eaf12f8168d21540aac5a5b8c32b41b6897c8141
Andreia de Lucas <adpdlucas@gmail.com>
2023-03-16 11:18:28 +0100

    Sender and receiver fixed position

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -4,7 +4,7 @@ import Rails from "@rails/ujs"
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
   static targets = ["messages", "form"]
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number }
 
   // connect() {
   //   console.log(`Subscribed to the chatroom with the id ${this.chatroomIdValue}.`)
@@ -39,8 +39,43 @@ export default class extends Controller {
   }
 
   // private
-  #insertMessageAndScrollDown(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
-    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+
+ // #insertMessageAndScrollDown(data) {
+    //console.log(data);
+    //const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    //this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    //this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+ // }
+
+ #insertMessageAndScrollDown(data) {
+  console.log(data);
+  // Logic to know if the sender is the current_user
+  const currentUserIsSender = this.currentUserIdValue === data.sender_id
+console.log(currentUserIsSender);
+  // Creating the whole message from the `data.message` String
+  const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+
+  // Inserting the `message` in the DOM
+  this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
+  this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+}
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
   }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender" : "receiver"
+  }
+
 }
